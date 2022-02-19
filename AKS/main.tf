c72f2cfb3a6c5362751be0d7f69bf9c8952bb024
ichiche <chicheng@microsoft.com>
2022-02-19 21:23:07 +0800

    Upload Files

---
+++ AKS/main.tf
@@ -0,0 +1,69 @@
+# Configure the Azure provider
+terraform {
+  required_version = "1.1.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.97"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = var.aks_resource_group_name
+  location = var.aks_location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                       = var.aks_name
+  location                   = var.aks_location
+  resource_group_name        = var.aks_resource_group_name
+  kubernetes_version         = var.kubernetes_version
+  dns_prefix_private_cluster = var.aks_dns_prefix 
+  local_account_disabled     = true
+  tags = {
+    Environment = "SIT"
+  }
+
+  default_node_pool {
+    name           = var.system_node_pool_name
+    node_count     = var.system_node_pool_vm_count
+    vm_size        = var.system_node_pool_vm_size
+    os_sku         = var.os_sku
+    os_disk_type   = var.os_disk_type 
+    vnet_subnet_id = var.system_node_pool_subnet_id
+    max_pods       = var.system_node_pool_max_pods 
+    availability_zones = [1,2,3]
+    type = "VirtualMachineScaleSets"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  azure_active_directory {
+    managed = true
+    azure_rbac_enabled = true
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_mode   = "transparent"
+  }
+  
+  oms_agent {
+    enabled = true
+    log_analytics_workspace_id = var.log_workspace_id
+  }
+}
---
+++ AKS/variables.tf
@@ -0,0 +1,63 @@
+variable "aks_resource_group_name" {
+  default = "AKS-Demo"
+  type    = string
+}
+
+variable "aks_location" {
+  default = "southeastasia"
+  type    = string
+}
+
+variable "aks_name" {
+  default = "aks-sit-sea-001"
+  type    = string
+}
+
+variable "kubernetes_version" {
+  default = "1.22.4"
+  type    = string
+}
+
+variable "aks_dns_prefix" {
+  default = "ichichedemo"
+  type    = string
+}
+
+variable "system_node_pool_name" {
+  default = "SystemNodePool"
+  type    = string
+}
+
+variable "system_node_pool_vm_count" {
+  default = 2
+  type    = number
+}
+
+variable "system_node_pool_vm_size" {
+  default = "Standard_D2s_v4"
+  type    = string
+}
+
+variable "os_sku" {
+  default = "Ubuntu"
+  type    = string
+}
+
+variable "os_disk_type" {
+  default = "Ephemeral"
+}
+
+variable "system_node_pool_subnet_id" {
+  default = "/subscriptions/7a2dec40-395f-45a9-b6b0-bef1593ce760/resourceGroups/Network/providers/Microsoft.Network/virtualNetworks/vn-sit-hk-peak/subnets/subnet-sit-hk-peak-aks"
+  type    = string
+}
+
+variable "system_node_pool_max_pods " {
+  default = 80
+  type    = number
+}
+
+variable "log_workspace_id" {
+  default = "/subscriptions/7a2dec40-395f-45a9-b6b0-bef1593ce760/resourceGroups/Log/providers/Microsoft.OperationalInsights/workspaces/log-analytics-aks-prd-sea-001"
+  type    = string
+}
