ee9717e36ae1f977890639bbb2fc180e98389497
ichiche <chicheng@microsoft.com>
2022-02-20 17:07:59 +0800

    Update outputs.tf

--- AKS/outputs.tf
+++ AKS/outputs.tf
@@ -1,8 +1,4 @@
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
-}
-
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
-  sensitive = true
+  sensitive = false
 }
