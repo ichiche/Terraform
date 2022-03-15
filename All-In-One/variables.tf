4901ee5079b20ea01ce4547ea237a9c676abb3ab
ichiche <chicheng@microsoft.com>
2022-03-15 12:43:20 +0800

    Update All-In-One v0.1.2

--- All-In-One/variables.tf
+++ All-In-One/variables.tf
@@ -1,3 +1,172 @@
+variable "wafpol-sit-hk-peak-i1_rg_location_var" {
+  description = "Azure Region of Resource Group of WAF Policy"
+  type    = string
+  default = "East Asia"
+}
+
+variable "wafpol-sit-hk-peak-i1_rg_name_var" {
+  description = "Resource Group of the WAF Policy"
+  type    = string
+  default = "rg-sit-hk-peak-app-gateway"
+}
+
+variable "wafpol-sit-hk-peak-i1_firewallpolicy_name_var" {
+  description = "Name of WAF Policy"
+  type    = string
+  default = "wafpol-sit-hk-peak-i1"
+}
+
+variable "wafpol-sit-hk-peak-i1_firewallpolicy_location_var" {
+  description = "Azure Region of WAF Policy"
+  type    = string
+  default = "East Asia"
+}
+
+variable "wafpol-sit-hk-peak-i1_custom_rules_var" {
+  default = [
+    {
+      action = "Block"
+      name                       = "sample-customrule1"
+      priority                   = 100
+      rule_type                  = "MatchRule"
+      match_conditions           = [{
+        operator           = "IPMatch"
+        negation_condition = false
+        match_values       = ["8.8.8.8"]
+        match_variables = [{
+          variable_name = "RequestHeaders"
+          selector = "UserAgent"
+        }]
+      }]
+    }
+  ]
+  type    = list
+}
+
+variable "wafpol-sit-hk-peak-i1_policysetting_enabled" {
+  description = "policy setting - enable"
+  default = true
+}
+
+variable "wafpol-sit-hk-peak-i1_policysetting_mode" {
+  description = "policy setting - mode"
+  default = "Detection"
+  type    = string
+}
+
+variable "wafpol-sit-hk-peak-i1_policysetting_request_body_check" {
+  description = "policy setting - request_body_check"
+  default = true
+}
+
+variable "wafpol-sit-hk-peak-i1_policysetting_mode_file_upload_limit_in_mb" {
+  description = "policy setting - file_upload_limit_in_mb"
+  default = "100"
+  type    = string
+}
+
+variable "wafpol-sit-hk-peak-i1_policysetting_mode_max_request_body_size_in_kb" {
+  description = "policy setting - max_request_body_size_in_kb"
+  default = "128"
+  type    = string
+}
+
+variable "agw-sit-hk-peak-i1_location" {
+  type    = string
+  default = "East Asia"
+}
+
+variable "agw-sit-hk-peak-i1_name_var" {
+  type    = string
+  default = "agw-sit-hk-peak-i1"
+}
+
+variable "agw-sit-hk-peak-i1_publicip_name" {
+  type    = string
+  default = "pip-agw-sit-hk-peak-i1"
+}
+
+# autoscale_configuration (min 6, max 40)
+variable "agw-sit-hk-peak-i1_sku_capability_min" {
+  type    = string
+  default = "6"
+}
+
+variable "agw-sit-hk-peak-i1_sku_capability_max" {
+  type    = string
+  default = "40"
+}
+
+variable "agw-sit-hk-peak-i1_gateway_ip_configuration_name" {
+  type    = string
+  default = "gateway-ip-configuration"
+}
+
+variable "agw-sit-hk-peak-i1_vnet_subnet_id" {
+  description = "agw-sit-hk-peak-i1 vnet subnet id"
+  type    = string
+  default = "/subscriptions/7a2dec40-395f-45a9-b6b0-bef1593ce760/resourceGroups/Network/providers/Microsoft.Network/virtualNetworks/vn-poc-hk-peak/subnets/subnet-poc-hk-peak-appgateway-02"
+}
+
+variable "agw-sit-hk-peak-i1_frontend_port" {
+  type    = string
+  default = "80"
+}
+
+variable "agw-sit-hk-peak-i1_frontend_private_ip" {
+  type    = string
+  default = "10.1.18.4"
+}
+
+# dummy address
+variable "agw-sit-hk-peak-i1_backend_address_pool_var" {
+  type    = list
+  default = [
+    {
+      name = "agw-sit-hk-peak-i1-beap"
+      fqdns = ["8.8.8.8"]
+    }
+  ]
+}
+
+variable "agw-sit-hk-peak-i1_backend_http_settings_var" {
+  type    = list
+  default = [
+    {
+      name                  = "agw-sit-hk-peak-i1-be-htst"
+      cookie_based_affinity = "Disabled"
+      port                  = "80"
+      protocol              = "Http"
+      request_timeout       = "20"
+    }
+  ]
+}
+
+variable "agw-sit-hk-peak-i1_http_listener_var" {
+  type    = list
+  default = [
+    {
+      name                           = "agw-sit-hk-peak-i1-httplstn"
+      frontend_ip_configuration_name = "agw-sit-hk-peak-i1-feip-private"
+      frontend_port_name             = "agw-sit-hk-peak-i1-feport"
+      protocol                       = "Http"
+    }
+  ]
+}
+
+variable "agw-sit-hk-peak-i1_request_routing_rule_var" {
+  type    = list
+  default = [
+    {
+      name                       = "agw-sit-hk-peak-i1-rqrt"
+      rule_type                  = "Basic"
+      http_listener_name         = "agw-sit-hk-peak-i1-httplstn"
+      backend_address_pool_name  = "agw-sit-hk-peak-i1-beap"
+      backend_http_settings_name = "agw-sit-hk-peak-i1-be-htst"
+    }
+  ]
+}
+
 variable "private_dns_zone_resource_group_name" {
   description = "Resource Group of Private DNS Zone"
   type    = string
@@ -93,13 +262,6 @@ variable "aks_vnet_id" {
   default = "/subscriptions/7a2dec40-395f-45a9-b6b0-bef1593ce760/resourceGroups/Network/providers/Microsoft.Network/virtualNetworks/vn-poc-hk-peak"
 }
 
-variable "application_gateway_id" {
-  description = "Resource Id of Application Gateway for Ingress Controller"
-  type    = string
-  #default = "/subscriptions/7a2dec40-395f-45a9-b6b0-bef1593ce760/resourceGroups/AppGateway/providers/Microsoft.Network/applicationGateways/agw-core-prd-sea-001"
-  default = "/subscriptions/7a2dec40-395f-45a9-b6b0-bef1593ce760/resourceGroups/rg-sit-hk-peak-app-gateway/providers/Microsoft.Network/applicationGateways/agw-sit-hk-peak-i1"
-}
-
 variable "log_workspace_id" {
   description = "Resource Id of Log Analytics Workspace for Insight"
   type    = string
@@ -137,7 +299,7 @@ variable "system_node_pool_name" {
 variable "system_node_pool_min_count" {
   description = "With Node Auto Scale enabled, minimum number of node. Recommend at least 2 nodes for System Node Pool"
   type    = number
-  default = 2
+  default = 1
 }
 
 variable "system_node_pool_max_count" {
